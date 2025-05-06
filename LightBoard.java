c040f46538ae956f1d67257c7f5710cc04d8963a
daphnet9 <daphnet9@nycstudents.net>
2025-05-06 02:55:25 +0000

    test

--- LightBoard.java
+++ LightBoard.java
@@ -8,12 +8,38 @@ public class LightBoard
  * Postcondition: each light has a 40% probability of being set to on.
  */
  public LightBoard(int numRows, int numCols)
- { /* to be implemented in part (a) */ }
+ {
+    lights = new boolean[numRows][numCols];
+    for(int r = 0; r < lights.length; r++)
+        for(int c = 0; c < lights[0].length; c++)
+            if(Math.random() < 0.4) lights[r][c] = true;
+ }
  /** Evaluates a light in row index row and column index col and returns a status
  * as described in part (b).
  * Precondition: row and col are valid indexes in lights.
  */
  public boolean evaluateLight(int row, int col)
- { /* to be implemented in part (b) */ }
+ {
+    boolean light = lights[row][col];
+    int colLights = 0;
+    for(int r = 0; r < lights.length; r++)
+        if(lights[r][col]) colLights++;
+        if(light && colLights % 2 == 0) return false;
+        if(!light && colLights % 3 == 0) return true;
+    return light;
+ }
  // There may be additional instance variables, constructors, and methods not shown.
+ public double test()
+ {
+    double on = 0;
+    for(boolean[] row : lights)
+        for(boolean b : row)
+            if(b) on++;
+    return on / (lights.length * lights[0].length);
+ }
+ 
+ public void setLights(boolean[][] arr)
+ {
+    lights = arr;
+ }
 } 
